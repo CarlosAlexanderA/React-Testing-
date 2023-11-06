cbdc14e92ddfb84114ecba50a1f2ababdea58519
CarlosAlexanderA <acerohuarahuara7551@gmail.com>
2023-11-05 20:46:55 -0500

    termianando la clase 1 de react

--- projects/00-hola-mundo/src/assets/App.jsx
+++ projects/00-hola-mundo/src/assets/App.jsx
@@ -1,26 +1,84 @@
 // importatar css de app
 import './App.css';
 import { TwitterFollowCard } from './TwitterFollowCard';
+// import { useState } from 'react';
 
 // creando un componente
 export const App = () => {
   const format = (userName) => `@${userName}`; // podermos pasar funciones
   // const formateddName = <span>@midudev</span> // podemos pasar elelmento tambien
+  // const pherald = {
+  //   // mala practica <- puede no llegar a entenderse
+  //   userName: 'pherald',
+  //   formatUserName: format,
+  //   initialIsFollowing: false,
+  // }; // se puede pasar objetos
+  // // propagaciond e cambios
+  // const [name, setName] = useState('midudev'); // permite actualizar todos los datos del componente
+  // const [isFollowing, setIsFollowing] = useState(true);
+  // console.log('initial following', isFollowing);
+
+  const users = [
+    {
+      userName: 'midudev',
+      name: 'miguel algel duran',
+      isFollowing: true,
+      formatUserName: format,
+    },
+    {
+      userName: 'pherald',
+      name: 'PabloH.',
+      isFollowing: false,
+      formatUserName: format,
+    },
+    {
+      userName: 'PacoHdezs',
+      name: 'Paco Hdez',
+      isFollowing: true,
+      formatUserName: format,
+    },
+    {
+      userName: 'TNChein',
+      name: 'Tomas',
+      isFollowing: false,
+      formatUserName: format,
+    },
+  ];
+
   return (
     // remplaza a React.Fragment '<>'
+    // todos los valores con los que se inician, ya no se actualizan por el padre, solo por el componente mismo
     <section className="App">
-      <TwitterFollowCard
+      {/* initialIsFollowing => solo se usa la primera vez y el padre no puede cambiar el valor, solo el componenete  */}
+      {/* <TwitterFollowCard
         formatUserName={format}
-        userName="midudev"
+        userName={name}
         name="Miguel Angel Durán"
-        isFollow
-      />
-      <TwitterFollowCard
-        formatUserName={format}
-        userName="pjerald"
-        name="Pablo Hérnandez"
-        isFollow={false}
-      />
+        initialIsFollowing={isFollowing}
+      >
+        <h1>Migel Angel Duran</h1>
+      </TwitterFollowCard> */}
+
+      {/* <TwitterFollowCard {...pherald}>Pablo Hernandez</TwitterFollowCard> */}
+
+      {/* <button onClick={() => setName('pedroMichel')}> Cambio nombre </button>
+      <button onClick={() => setIsFollowing(!isFollowing)}>
+        Cambio de estado
+      </button> */}
+
+      {users.map((user) => {
+        const { userName, name, isFollowing, formatUserName } = user;
+        return (
+          <TwitterFollowCard
+            key={userName}
+            userName={userName}
+            initialIsFollowing={isFollowing}
+            formatUserName={formatUserName}
+          >
+            {name}
+          </TwitterFollowCard>
+        );
+      })}
     </section>
   );
 };
--- projects/00-hola-mundo/src/assets/TwitterFollowCard.jsx
+++ projects/00-hola-mundo/src/assets/TwitterFollowCard.jsx
@@ -1,9 +1,29 @@
+// hooks, para darle mas funcionalidad a react 'utilities'
+import { useState } from 'react';
+// todas las props que se inician solo se cambian con este component, ya no por el padre
 export const TwitterFollowCard = ({
+  children,
   formatUserName,
   userName,
   name,
-  isFollow,
+  initialIsFollowing,
 }) => {
+  // const state = useState(false) ; // deveulve un array de 2
+  // const isFollowing = state[0] // estado en el que se encuentra
+  // const setIsFollwing = state[1] // para cambiar el estado
+
+  const [isFollowing, setIsFollwing] = useState(initialIsFollowing); // forma corta
+
+  const handleClick = () => {
+    setIsFollwing(!isFollowing);
+  };
+  // cambiamos el estado de nuestro componente
+  const text = isFollowing ? 'Siguiendo' : 'Seguir';
+  // las clases segun nuestro estado
+  const buttonClassName = isFollowing
+    ? 'tw-follwCard-button is-following'
+    : 'tw-follwCard-button';
+
   return (
     <article className="tw-follwCard">
       <header className="tw-follwCard-header">
@@ -13,12 +33,15 @@ export const TwitterFollowCard = ({
           alt="avatar de perfil"
         />
         <div className="tw-follwCard-info">
-          <strong>{name}</strong>
+          <strong>{children === undefined ? name : children}</strong>
           <span>{formatUserName(userName)}</span>
         </div>
       </header>
       <aside>
-        <button className="tw-follwCard-button">Seguir</button>
+        <button className={buttonClassName} onClick={handleClick}>
+          <span className="tw-follwCard-text">{text}</span>
+          <span className="tw-follwCard-stopFollow">Dejar de seguir</span>
+        </button>
       </aside>
     </article>
   );
