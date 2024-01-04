45cd357e408b6743b0f3c9b6320bfc44bd6d35d0
CarlosAlexanderA <acerohuarahuara7551@gmail.com>
2024-01-03 20:52:28 -0500

    Add boilerplate for project 12

---
+++ projects/12-comments-react-query/src/App.tsx
@@ -0,0 +1,44 @@
+import {useQuery} from '@tanstack/react-query';
+import {FormInput, FormTextArea} from './components/Form';
+import {Results} from './components/Results';
+import {CommentWithId, getComments} from './service/comments';
+
+function App() {
+  const {data, isLoading, isError} = useQuery<CommentWithId>({
+    queryKey: ['comments'],
+    queryFn: getComments,
+  });
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+
+  return (
+    <main className="grid grid-cols-2 h-screen">
+      <div className="col-span-1 bg-white p-8">
+        {isLoading && <strong>Cargando...</strong>}
+        {isError && <strong>Algo salio mal</strong>}
+        <Results data={data ? [data] : undefined} />
+      </div>
+      <div className="col-span-1 bg-gray-800 p-8">
+        <form
+          action="#"
+          className="max-w-xl m-auto block px-4"
+          onSubmit={handleSubmit}
+        >
+          <FormInput />
+          <FormTextArea />
+
+          <button
+            type="submit"
+            className="mt-4 px-12 text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm py-2.5 text-center mr-2 mb-2"
+          >
+            Enviar comentario
+          </button>
+        </form>
+      </div>
+    </main>
+  );
+}
+
+export default App;
---
+++ projects/12-comments-react-query/src/service/comments.ts
@@ -0,0 +1,44 @@
+export interface Comment {
+  title: string;
+  message: string;
+}
+
+export interface CommentWithId extends Comment {
+  id: string;
+}
+const API_KEY = import.meta.env.VITE_API_KEY_JSON_BIN;
+const API_URL = 'https://api.jsonbin.io/v3/b/6595d7691f5677401f17260d';
+export const getComments = async () => {
+  const response = await fetch(API_URL, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Access-Key': API_KEY,
+    },
+  });
+
+  if (!response.ok) throw new Error('Failed to fetch comments.');
+
+  const json = await response.json();
+  return json?.record;
+};
+export const postComment = async (comment: Comment) => {
+  const comments = await getComments();
+
+  const id = crypto.randomUUID();
+  const newComment = {...comment, id};
+  const commentsToSave = [...comments, newComment];
+
+  const response = await fetch(API_URL, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Access-Key': API_KEY,
+    },
+    body: JSON.stringify(commentsToSave),
+  });
+
+  if (!response.ok) throw new Error('Failed to post comment.');
+
+  return newComment;
+};
